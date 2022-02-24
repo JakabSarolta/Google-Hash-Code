f879659f0afb409dde8febd88d2465f6fcd980c3
biromp <biromp6@protonmail.com>
2022-02-24 21:50:52 +0200

    fyyjhj

--- hash/main.cpp
+++ hash/main.cpp
@@ -34,11 +34,34 @@ int main()
 
         f>>name>>skillsNr;
 
+        cout << "\nContributor " << i << ": " << name << " " << skillsNr << "\n";
+
+
         for (int j=0; j<skillsNr; j++)
         {
             f>>skillName>>skillLevel;
+            cout << "   " << skillName << " " << skillLevel << "\n";
+            //pushSkill(name, skillName, skillLevel);
+        }
+    }
 
-            pushSkill(name, skillName, skillLevel);
+    cout << "\n\n---------------------------------\n\n";
+
+    //projects
+    for (int i=0; i<nr_of_projects; i++)
+    {
+        string projName, skillName;
+        int days, score, bbefore, roles, skillLevel;
+
+        f>>projName>>days>>score>>bbefore>>roles;
+
+        cout << "\nProject " << i << ": " << projName << "\n SkillNr: " << roles << "\n Days: " << days << "\n BBefore: " << bbefore << "\n Score: " << score << "\nSkills: \n";
+
+        for (int j=0; j<roles; j++)
+        {
+            f>>skillName>>skillLevel;
+            cout << "   " << skillName << " " << skillLevel << "\n";
+//            pushProject(projName, skillName, skillLevel);
         }
     }
 
