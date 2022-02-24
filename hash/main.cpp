82db74eb3b4430d44612cd5605058df13a32b090
pizzahunter2000 <lorandszabo02@gmail.com>
2022-02-24 21:21:23 +0200

    skill struct

--- hash/main.cpp
+++ hash/main.cpp
@@ -2,21 +2,18 @@
 
 using namespace std;
 
-struct Skill{
-    char name[20];
-    short level;
-};
-
-struct Contributor{
-    char name[20];
-    Skill skills[1000];
-};
-
 struct Project{
     char name[20];
     int nr_of_days, score, nr_of_roles, best_before;
-    Skill skills[1000];
 };
+
+struct Skill{
+    char name[20];
+    short level;
+    Project needTo[1000];
+    string contributorWith[1000];
+};
+
 
 int main()
 {
