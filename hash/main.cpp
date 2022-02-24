d1f0f8bd965cb17bda7bf3f1ae8bfc595e440d30
pizzahunter2000 <lorandszabo02@gmail.com>
2022-02-24 21:10:40 +0200

    first 'real' commit

--- hash/main.cpp
+++ hash/main.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+struct Skill{
+    char name[20];
+    short level;
+};
+
+struct Contributor{
+    char name[20];
+    Skill skills[1000];
+};
+
+struct Project{
+    char name[20];
+    int nr_of_days, score, nr_of_roles, best_before;
+    Skill skills[1000];
+};
+
 int main()
 {
     cout << "Hey";
